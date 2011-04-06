917bd39419683be4dc3c9b00426705ebe689193f
Ben Hamill <git-commits@benhamill.com>
2011-04-06 00:50:10 -0500

    SASS.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Sass::Plugin.options[:style] = :compact
+Sass::Plugin.options[:template_location] = "#{Rails.root}/app/views/stylesheets"
+Sass::Plugin.options[:css_location] = "#{Rails.root}/public/stylesheets"
+
 # Initialize the rails application
 Heygovote::Application.initialize!
