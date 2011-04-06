4553a2d0361e79bded8373d22da38b3e642444bf
Ben Hamill <git-commits@benhamill.com>
2011-04-05 23:36:04 -0500

    Oh look! A blank rails app!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heygovote::Application.config.secret_token = '206c78430a37509bd44dfed86d0bb40d56f333ef5e6d0804afd0534a014d92f97a38bbab3338188103e418c65f5373aba153b15d385a3be6215c67ae06bab505'
