f26974860eba8bdff166b7ec7d87ae3ddcdcca49
Ben Hamill <git-commits@benhamill.com>
2011-04-06 02:09:06 -0500

    Make devise happy about emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Heygovote::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'heygovote.com' }
 end
